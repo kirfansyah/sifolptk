5008a5623f2a303bcffa0fe73659e078b846088f
unknown <kikiirfansyah33@gmail.com>
2023-01-17 15:47:06 +0700

    update database

--- db/db_sifolptk.sql
+++ db/db_sifolptk.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Jan 2023 pada 16.44
--- Versi server: 10.4.19-MariaDB
--- Versi PHP: 7.4.20
+-- Waktu pembuatan: 17 Jan 2023 pada 09.45
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,85 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_layanan`
+--
+
+CREATE TABLE `tb_layanan` (
+  `id` int(11) NOT NULL,
+  `layanan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_layanan`
+--
+
+INSERT INTO `tb_layanan` (`id`, `layanan`) VALUES
+(2, 'Layanan 1'),
+(3, 'Layanan 2'),
+(4, 'Layanan 3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pegawai`
+--
+
+CREATE TABLE `tb_pegawai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `nik` varchar(128) NOT NULL,
+  `no_hp` varchar(128) NOT NULL,
+  `alamat` text NOT NULL,
+  `jabatan` varchar(128) NOT NULL,
+  `tgl_lahir` varchar(128) NOT NULL,
+  `tempat_lahir` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_pegawai`
+--
+
+INSERT INTO `tb_pegawai` (`id`, `nama`, `nik`, `no_hp`, `alamat`, `jabatan`, `tgl_lahir`, `tempat_lahir`) VALUES
+(8, 'Pegawai 1', '1111111111111111', '2222222222222', 'tes', 'tes', '18 January, 2023', 'tes'),
+(9, 'Pegawai 2', '1111111111111111', '2222222222222', 'medan', 'tes', '18 January, 2023', 'medan'),
+(10, 'Pegawai 3', '1111111111111111', '2222222222222', 'tes', 'tes', '17 January, 2023', 'tes');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_registrasi`
+--
+
+CREATE TABLE `tb_registrasi` (
+  `id` int(11) NOT NULL,
+  `id_pegawai` int(11) DEFAULT NULL,
+  `id_layanan` int(11) DEFAULT NULL,
+  `nama` varchar(128) DEFAULT NULL,
+  `no_hp` varchar(128) DEFAULT NULL,
+  `no_reg` varchar(128) DEFAULT NULL,
+  `tanggal_registrasi` date DEFAULT NULL,
+  `sts_app` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_registrasi`
+--
+
+INSERT INTO `tb_registrasi` (`id`, `id_pegawai`, `id_layanan`, `nama`, `no_hp`, `no_reg`, `tanggal_registrasi`, `sts_app`) VALUES
+(1, 8, 2, 'CMP', '2222222222222', 'PTK17012023', '2023-01-17', NULL),
+(2, 8, 2, 'CMP', '2222222222222', 'PTK117012023', '2023-01-17', NULL),
+(3, 8, 2, 'CMP', '2222222222222', 'PTK217012023', '2023-01-17', NULL),
+(4, 8, 2, 'CMP', '2222222222222', 'PTK317012023', '2023-01-17', NULL),
+(5, 8, 2, 'CMP', '2222222222222', 'PTK417012023', '2023-01-17', NULL),
+(6, 8, 2, 'CMP', '4444444444444', 'PTK517012023', '2023-01-17', NULL),
+(7, 8, 2, 'CMP', '2222222222222', 'PTK617012023', '2023-01-17', NULL),
+(8, 8, 2, 'CMP', '2222222222222', 'PTK717012023', '2023-01-17', NULL),
+(9, 8, 2, 'CMP', '2222222222222', 'PTK817012023', '2023-01-17', NULL),
+(10, 9, 2, 'CMP', '2222222222222', 'PTK917012023', '2023-01-17', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -38,6 +117,13 @@ CREATE TABLE `user` (
   `date_created` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(4, 'Kiki Irvansyah', 'kikiirfansyah30@gmail.com', 'default.jpg', '$2y$10$Vd5ASEgUiSVc5V77M40p6.51QPE41EHcC2APOyzDdquOjJOdPXAp.', 1, 1, 1673872155);
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +136,15 @@ CREATE TABLE `user_access_menu` (
   `menu_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -58,17 +153,18 @@ CREATE TABLE `user_access_menu` (
 
 CREATE TABLE `user_menu` (
   `id` int(11) NOT NULL,
-  `menu` varchar(128) NOT NULL
+  `menu` varchar(128) NOT NULL,
+  `icon_menu` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user_menu`
 --
 
-INSERT INTO `user_menu` (`id`, `menu`) VALUES
-(1, 'Master'),
-(2, 'Data Harian'),
-(3, 'Approval');
+INSERT INTO `user_menu` (`id`, `menu`, `icon_menu`) VALUES
+(1, 'M', 'icon-check-square'),
+(2, 'D', 'icon-check-square'),
+(3, 'A', 'icon-check-square');
 
 -- --------------------------------------------------------
 
@@ -109,11 +205,34 @@ CREATE TABLE `user_sub_menu` (
 --
 
 INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
-(1, 1, 'User', '', '', 1);
+(1, 1, 'User', 'user/', 'icon-user', 1),
+(2, 1, 'Role Akses', 'user/role', 'icon-users', 1),
+(3, 1, 'Data Pegawai', 'pegawai/', 'icon-users', 1),
+(4, 2, 'Cek Data Harian', '', 'icon-users', 1),
+(5, 3, 'Approval', '', 'icon-users', 1),
+(6, 1, 'Layanan', 'layanan/', '\r\nicon-file-minus', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `tb_layanan`
+--
+ALTER TABLE `tb_layanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_pegawai`
+--
+ALTER TABLE `tb_pegawai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_registrasi`
+--
+ALTER TABLE `tb_registrasi`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `user`
@@ -150,16 +269,34 @@ ALTER TABLE `user_sub_menu`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `tb_layanan`
+--
+ALTER TABLE `tb_layanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pegawai`
+--
+ALTER TABLE `tb_pegawai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_registrasi`
+--
+ALTER TABLE `tb_registrasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_menu`
@@ -177,7 +314,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT untuk tabel `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
