cd6471ebe6ee4eeef46addadbd41a69ef5832f8c
kiki1897 <kikiirfansyah33@gmail.com>
2023-01-29 13:39:02 +0700

    fix login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
@@ -11,30 +10,30 @@
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>Login Page - Vuexy - Bootstrap HTML admin template</title>
-    <link rel="apple-touch-icon" href="<?= base_url()?>/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url()?>/app-assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon" href="<?= base_url() ?>/app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>/app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/vendors/css/vendors.min.css">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/themes/semi-dark-layout.css">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/core/menu/menu-types/horizontal-menu.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/core/colors/palette-gradient.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/app-assets/css/pages/authentication.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/core/menu/menu-types/horizontal-menu.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/app-assets/css/pages/authentication.css">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/style.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -56,7 +55,7 @@
                         <div class="card bg-authentication rounded-0 mb-0">
                             <div class="row m-0">
                                 <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
-                                    <img src="<?= base_url()?>/app-assets/images/pages/login.png" alt="branding logo">
+                                    <img src="<?= base_url() ?>/app-assets/images/pages/login.png" alt="branding logo">
                                 </div>
                                 <div class="col-lg-6 col-12 p-0">
                                     <div class="card rounded-0 mb-0 px-2">
@@ -65,7 +64,7 @@
                                                 <h4 class="mb-0">Login</h4>
                                             </div>
                                         </div>
-                                        <p class="px-2">Selamat datang, silakan login ke akunmu.</p>
+                                        <p class="px-2">Selamat datang, silakan login ke akunmu atau kembali ke <a href="<?= base_url() ?>">Halaman utama.</a></p>
                                         <div class="card-content">
                                             <div class="card-body pt-1">
                                                 <?php echo $this->session->flashdata('message'); ?>
@@ -76,7 +75,7 @@
                                                             <i class="feather icon-user"></i>
                                                         </div>
                                                         <label for="user-name">Email</label>
-                                                        <?php echo form_error('email', '<small class="text-danger pl-3">','</small>'); ?>
+                                                        <?php echo form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                                     </fieldset>
 
                                                     <fieldset class="form-label-group position-relative has-icon-left">
@@ -85,14 +84,14 @@
                                                             <i class="feather icon-lock"></i>
                                                         </div>
                                                         <label for="user-password">Password</label>
-                                                        <?php echo form_error('password', '<small class="text-danger pl-3">','</small>'); ?>
+                                                        <?php echo form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                                     </fieldset>
                                                     <div class="form-group d-flex justify-content-between align-items-center">
                                                         <div class="text-left">
-                                                           
+
                                                         </div>
-                                                        
-                                                    </div>                                                    
+
+                                                    </div>
                                                     <button type="submit" class="btn btn-primary float-center btn-inline">Login</button>
                                                 </form>
                                             </div>
@@ -101,7 +100,7 @@
                                             <div class="divider">
                                                 <div class="divider-text"></div>
                                             </div>
-                                          
+
                                         </div>
                                     </div>
                                 </div>
@@ -117,17 +116,17 @@
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="<?= base_url()?>/app-assets/vendors/js/vendors.min.js"></script>
+    <script src="<?= base_url() ?>/app-assets/vendors/js/vendors.min.js"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="<?= base_url()?>/app-assets/vendors/js/ui/jquery.sticky.js"></script>
+    <script src="<?= base_url() ?>/app-assets/vendors/js/ui/jquery.sticky.js"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="<?= base_url()?>/app-assets/js/core/app-menu.js"></script>
-    <script src="<?= base_url()?>/app-assets/js/core/app.js"></script>
-    <script src="<?= base_url()?>/app-assets/js/scripts/components.js"></script>
+    <script src="<?= base_url() ?>/app-assets/js/core/app-menu.js"></script>
+    <script src="<?= base_url() ?>/app-assets/js/core/app.js"></script>
+    <script src="<?= base_url() ?>/app-assets/js/scripts/components.js"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
